474ef3b0ee525e3a9c19ff5c9cca2df06ac1d477
Ibrokhim <86555724+REACT-DEVELOPER-IBROKHIM@users.noreply.github.com>
2023-04-26 11:37:02 +0500

    Binary setup completed

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// Please use CSS module
+
+const Footer = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from "./Header.module.scss";
+// Please use CSS module
+
+const Header = () => {
+  return (
+    <div className={styles.header}>
+      
+    </div>
+  )
+}
+
+export default Header
