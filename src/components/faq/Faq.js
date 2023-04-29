2f9541c56ff956ee8695ff8e6ddcf98d74438e18
Jaloliddin-Gulomov <jaloliddingulomov7061@gmail.com>
2023-04-30 00:21:26 +0500

    added emojies

--- src/components/faq/Faq.js
+++ src/components/faq/Faq.js
@@ -2,18 +2,20 @@ import React from 'react';
 import style from "./Faq.module.scss";
 import Accordion from './accordion/Accordion';
 import accordionData from './accordion/AccordionData.json';
+import message from '../../essets/message.svg'
+import finger from '../../essets/finger.svg'
 function Faq() {
     return (
         <div className={style.faq}>
             <div className={style.faq__child}>
-                <h2>Имея при себе много опыта, мы можем помочь в процветании <u>вашего</u> бизнеса</h2>
+                <h2>Имея при себе много опыта, мы можем помочь в процветании <u>вашего</u> <img src={finger} alt='finger' /> бизнеса</h2>
                 <div className={style.accordion}>
                     {accordionData.map(({ title, content }) => (
                         <Accordion title={title} content={content} />
                     ))}
                 </div>
             </div>
-            <button className='btns'><span>Оставить заявку</span></button>
+            <button className='btns'><span>Оставить заявку</span><img src={message} alt='Message' /></button>
         </div>
     );
 }
