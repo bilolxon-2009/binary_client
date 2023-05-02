37d502fbe787c9b725b30f6e258a35dfb243ef10
Jaloliddin-Gulomov <jaloliddingulomov7061@gmail.com>
2023-05-02 13:07:26 +0500

    commit

--- src/components/case/Case.js
+++ src/components/case/Case.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import style from "./Case.module.scss"
+import family from '../../essets/family.svg'
 function Case() {
     return (
-        <div className={style.case}>Case</div>
+        <div className={style.case}>
+            <h2 className={style.title}>Мы выросли из компании семейного <img src={family} alt="family" /> типа в 🏙️ компанию, успешно консультирующую многие бренды и цифровые продукты на рынке</h2>
+        </div>
     )
 }
 
