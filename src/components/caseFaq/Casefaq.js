d520606e14c25945546a3e1f56cc32fc4a809af5
Jaloliddin-Gulomov <jaloliddingulomov7061@gmail.com>
2023-05-12 19:51:39 +0500

    edited caseselection and casefaq

--- src/components/caseFaq/Casefaq.js
+++ src/components/caseFaq/Casefaq.js
@@ -1,24 +1,11 @@
 import React from 'react'
 import style from "./Casefaq.module.scss"
-import Accordion from './accordion/Accordion';
-import accordionData from './accordion/AccordionData.json';
-import finger from '../../essets/finger.svg'
-import { Container } from '../../utils/Components';
 import ScrollRoute from '../../utils/scrollroute/Scrollroute';
+import Faq from '../faq/Faq';
 function Casefaq() {
     return (
         <div className={style.casefaq} id='casefaq'>
-            <Container>
-                <div className={style.casefaq__child}>
-                    <h2>Имея при себе много опыта, мы можем помочь в процветании <u>вашего</u> <img src={finger} alt='finger' /> бизнеса</h2>
-                    <div className={style.casefaq__accordion}>
-                        {accordionData.map(({ title, content, id }) => (
-                            <Accordion title={title} content={content} key={id} />
-                        ))}
-                    </div>
-                </div>
-                <button className='btns'><span>Наше расположение</span></button>
-            </Container>
+          <Faq/>
             <ScrollRoute routeName={"casefaq"} />
 
         </div>
