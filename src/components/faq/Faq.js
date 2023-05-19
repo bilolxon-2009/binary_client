48828fe21069fe567715ee68d865d3267432f9e6
Jaloliddin-Gulomov <jaloliddingulomov7061@gmail.com>
2023-05-19 15:17:18 +0500

    added faq media

--- src/components/faq/Faq.js
+++ src/components/faq/Faq.js
@@ -5,7 +5,6 @@ import accordionData from './accordion/AccordionData.json';
 import message from '../../essets/message.svg'
 import finger from '../../essets/finger.svg'
 import ScrollRoute from '../../utils/scrollroute/Scrollroute';
-import { Container } from '../../utils/Components';
 import { useLocation } from 'react-router-dom';
 
 function Faq({ dark }) {
@@ -26,7 +25,6 @@ function Faq({ dark }) {
     return (
         <div className={`${dark ? style.faq__dark : style.faq__light}`} id={idRoute}>
             <div className={style.faq}>
-                <Container>
                     <div className={style.faq__child}>
                         <h2>Имея при себе много опыта, мы можем помочь в процветании <u>вашего</u> <img src={finger} alt='finger' /> бизнеса</h2>
                         <div className={style.accordion}>
@@ -36,7 +34,6 @@ function Faq({ dark }) {
                         </div>
                     </div>
                     <button className='btns'>{dark ? <div><span>Оставить заявку</span><img src={message} alt='Message' /></div> : <span>Наше расположение</span>}</button>
-                </Container>
                 <ScrollRoute routeName={idRoute} />
             </div>
         </div>
